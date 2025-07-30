84c33c6e00eea32eefe0756934a5d2ff0ba489f4
Jacky1998Hung <uuuwki2953@gmail.com>
2025-07-30 22:47:07 +0800

    observe *trace.txt for konata, see if i have successfully fetched only cache miss instruction

--- sim-outorder.c
+++ sim-outorder.c
@@ -4339,6 +4339,7 @@ ruu_fetch(void)
       fetch_data[fetch_tail].ptrace_seq = ptrace_seq++;
 
       /* for pipe trace */
+      //if(last_inst_missed || last_inst_tmissed) {
       ptrace_newinst(fetch_data[fetch_tail].ptrace_seq,
 		     inst, fetch_data[fetch_tail].regs_PC,
 		     0);
@@ -4346,6 +4347,7 @@ ruu_fetch(void)
 		      PST_IFETCH,
 		      ((last_inst_missed ? PEV_CACHEMISS : 0)
 		       | (last_inst_tmissed ? PEV_TLBMISS : 0)));
+      //}
       last_inst_missed = FALSE;
       last_inst_tmissed = FALSE;
 
